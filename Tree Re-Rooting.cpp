556416dc31ff4603bc17e171957cdf0c74df20bd
raj-orton <35659984+raj-orton@users.noreply.github.com>
2020-06-11 00:36:04 +0530

    Update Tree Re-Rooting.cpp

--- Tree Re-Rooting.cpp
+++ Tree Re-Rooting.cpp
@@ -66,7 +66,7 @@ void solve() {
 
 signed main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cerr.tie(0);
-	// int t; cin >> t; for(int i = 1; i <= t; i++) //cout << "Case #" << i << ": ",
+	// int t; cin >> t; for(int i = 1; i <= t; i++) //cout << "Case #" << i << ": ", //
 	solve();
 	return 0;
 }
