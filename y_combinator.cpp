b7af72e242bc2b4f3e80effa955781cacdb80447
raj-orton <35659984+raj-orton@users.noreply.github.com>
2021-06-03 04:31:14 +0530

    Update y_combinator.cpp

--- y_combinator.cpp
+++ y_combinator.cpp
@@ -1,17 +1,25 @@
-template<class Fun>
+template <class Fun>
 class y_combinator_result {
-	Fun fun_;
-public:
-	template<class T>
-	explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
- 
-	template<class ...Args>
-	decltype(auto) operator()(Args &&...args) {
-		return fun_(std::ref(*this), std::forward<Args>(args)...);
-	}
+    Fun fun_;
+
+  public:
+    template <class T>
+    explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+
+    template <class... Args>
+    decltype(auto) operator()(Args &&...args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
 };
- 
-template<class Fun>
+
+template <class Fun>
 decltype(auto) y_combinator(Fun &&fun) {
-	return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
 }
+
+/*
+auto functionName = y_combinator([captureVariable](auto functionName, ...args) -> returnType {
+	functionName(args);
+});
+functionName(args);
+*/
