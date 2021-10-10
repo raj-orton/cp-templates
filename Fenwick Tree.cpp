a5e546a55136f9ed0db3c9a753a1559c053d32c2
raj-orton <35659984+raj-orton@users.noreply.github.com>
2021-10-10 13:44:34 +0530

    Changed to class

--- Fenwick Tree.cpp
+++ Fenwick Tree.cpp
@@ -1,44 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-vector<long long> ft;
-
-long long query(int idx) {
-    long long ans = 0LL;
-    for(; idx; idx -= (idx & -idx))
-        ans += ft[idx];
-    return ans;
-}
-
-long long rangeQuery(int L, int R) {
-    return (query(R) - query(L - 1));
-}
-
-void update(int idx, long long val) {
-    for(; idx <= n; idx += (idx & -idx))
-        ft[idx] += val;
-}
-
-void build() {
-    ft.resize(n + 3, 0);
-    vector<long long> a(n + 3);
-    for(int i = 1; i <= n; i++) {
-        cin >> a[i];
-        update(i, a[i]);
+template <typename Type>
+struct FenwickTree {
+    int n;
+    vector<Type> ft;
+    
+    FenwickTree(int n_ = 100000) {
+        n = n_;
+        ft.resize(n + 1);
     }
-}
 
-int main() {
+    Type get(int idx) {
+        Type ans = 0LL;
+        for(; idx; idx -= (idx & -idx))
+            ans += ft[idx];
+        return ans;
+    }
 
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    Type get(int L, int R) {
+        return (get(R) - get(L - 1));
+    }
 
-    /*
-    1-based indexing
-    build();
-    update(index, value);
-    query(index); //Gives from 1 - index
-    */
-
-	return 0;
-}
+    void add(int idx, Type val) {
+        for(; idx <= n; idx += (idx & -idx))
+            ft[idx] += val;
+    }
+};
